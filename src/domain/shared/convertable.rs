c60b4c941754271b7aedf304d6d8c8ae73612378
Flávio Granato <flavio.granato@pm.me>
2025-06-15 17:58:49 -0300

    feature(task): Added task management

--- src/domain/shared/convertable.rs
+++ src/domain/shared/convertable.rs
@@ -1,8 +1,4 @@
-/// Trait para conversão entre diferentes representações de uma entidade
 pub trait Convertable<T> {
-    /// Converte a entidade para o tipo T
     fn to(&self) -> T;
-
-    /// Cria uma entidade a partir do tipo T
     fn from(source: T) -> Self;
 }
