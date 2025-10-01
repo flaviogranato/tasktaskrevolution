4e879ccdaaa573d4579aca2aebdaa3ea0182be81
Flávio Granato <flavio.granato@pm.me>
2025-10-01 15:41:45 -0300

    fix: convert DomainError to AppError in cancel_project.rs

--- src/application/project/cancel_project.rs
+++ src/application/project/cancel_project.rs
@@ -67,7 +67,7 @@ where
         let project_id = self
             .code_resolver
             .resolve_project_code(project_code)
-            .map_err(CancelAppError::RepositoryError)?;
+            .map_err(|e| CancelAppError::RepositoryError(AppError::from(e)))?;
 
         // 2. Load the project aggregate using ID
         let project = self
